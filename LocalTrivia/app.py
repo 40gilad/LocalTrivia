321a1b3dfd35aa718cd10a8ccc314ff857fb2610
40gilad <40gilad.shenkar@gmail.com>
2025-01-14 12:11:46 +0200

    more modifications

--- LocalTrivia/app.py
+++ LocalTrivia/app.py
@@ -9,8 +9,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-# Store player info in memory
+# Store player info and scores in memory
 players = {}
+scores = {}
+score_limit = 0
 current_buzzer = None
 
 # Ensure uploads directory exists
@@ -42,13 +44,23 @@ def register():
     with open(filepath, 'wb') as f:
         f.write(base64.b64decode(image_data))
 
-    # Store player info
+    # Store player info and initialize score
     players[request.remote_addr] = {
         'name': player_name,
         'image': f'/static/uploads/{filename}'
     }
+    scores[player_name] = 0
 
+    # Broadcast updated scores to all clients
+    emit('update_scores', scores, broadcast=True, namespace='/')
     return jsonify({'status': 'success'})
+
+
+@socketio.on('set_score_limit')
+def handle_score_limit(data):
+    global score_limit
+    score_limit = int(data['limit'])
+    emit('score_limit_set', {'limit': score_limit}, broadcast=True)
 
 
 @socketio.on('buzz')
@@ -61,6 +73,46 @@ def handle_buzz():
             'name': player['name'],
             'image': player['image']
         }, broadcast=True)
+
+
+@socketio.on('answer_result')
+def handle_answer(data):
+    global current_buzzer
+    if current_buzzer and data['correct']:
+        player_name = players[current_buzzer]['name']
+        scores[player_name] += 1
+
+        # Check for winner
+        if score_limit > 0 and scores[player_name] >= score_limit:
+            emit('game_won', {
+                'name': player_name,
+                'image': players[current_buzzer]['image'],
+                'score': scores[player_name]
+            }, broadcast=True)
+        else:
+            emit('update_scores', scores, broadcast=True)
+
+    current_buzzer = None
+    emit('buzzer_reset', broadcast=True)
+
+
+@socketio.on('update_manual_score')
+def handle_manual_score(data):
+    player_name = data['player']
+    new_score = int(data['score'])
+    if player_name in scores:
+        scores[player_name] = new_score
+
+        # Check for winner
+        if score_limit > 0 and scores[player_name] >= score_limit:
+            winner_addr = next(addr for addr, p in players.items() if p['name'] == player_name)
+            emit('game_won', {
+                'name': player_name,
+                'image': players[winner_addr]['image'],
+                'score': scores[player_name]
+            }, broadcast=True)
+        else:
+            emit('update_scores', scores, broadcast=True)
 
 
 @socketio.on('reset_buzzer')
